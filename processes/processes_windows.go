3147bbd37dd9eb04d585eee05391234d6703685b
jyogi <xu.xi@founder.com>
2017-03-10 09:31:28 +0800

    Update processes_windows.go

--- processes/processes_windows.go
+++ processes/processes_windows.go
@@ -1,7 +1,25 @@
 package processes
 
-import "errors"
+import (
+	//"os/exec"
+	"errors"
+	//"fmt"
+
+	//3p
+	//"github.com/axgle/mahonia"
+)
+
+//func getProcesses(limit int) ([]interface{}, error) {
+//	return nil, errors.New("Not implemented on Windows")
+//}
 
 func getProcesses(limit int) ([]interface{}, error) {
+	//exec.Command("chcp", "936").CombinedOutput()
+	//out, err := exec.Command("query", "process").CombinedOutput()
+	//if err != nil {
+	//	//return nil, errors.New("Not implemented on Windows")
+	//}
+	//var str string =string(mahonia.NewDecoder("gb18030").ConvertString(string(out)))
+	//fmt.Println("ProcessName : "+string(str));
 	return nil, errors.New("Not implemented on Windows")
 }
